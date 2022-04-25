4bdfbb2c5401d4be297eba6437c22b75ea692fa5
MorsCrt <203301130@ogr.selcuk.edu.tr>
2022-04-26 00:43:48 +0300

    fixing Dataframe keyword

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def lambda_handler(event,context):
     d = {'col1':[1,2]}
-    df = pd.Dataframe(data=d)
+    df = pd.DataFrame(data=d)
     print(df)
     print("Done")
     
